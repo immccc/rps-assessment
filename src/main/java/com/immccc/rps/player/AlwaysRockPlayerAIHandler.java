ee54519d0f2c542410fda90d2467460de90f3710
ïmmccc <immccc@gmail.com>
2018-09-29 10:37:13 +0200

    JSF starter

--- src/main/java/com/immccc/rps/player/AlwaysRockPlayerAIHandler.java
+++ src/main/java/com/immccc/rps/player/AlwaysRockPlayerAIHandler.java
@@ -1,10 +1,13 @@
 package com.immccc.rps.player;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Random;
 
 import static com.immccc.rps.player.PlayerAIHandlerType.ALWAYS_ROCK;
 import static com.immccc.rps.player.PlayerSelectionType.ROCK;
 
+@Service
 class AlwaysRockPlayerAIHandler implements PlayerAIHandler {
     private static final Random random = new Random();
 
--- src/main/java/com/immccc/rps/player/RandomPlayerAIHandler.java
+++ src/main/java/com/immccc/rps/player/RandomPlayerAIHandler.java
@@ -1,9 +1,12 @@
 package com.immccc.rps.player;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Random;
 
 import static com.immccc.rps.player.PlayerAIHandlerType.RANDOM;
 
+@Service
 class RandomPlayerAIHandler implements PlayerAIHandler {
     private static final Random random = new Random();
 
